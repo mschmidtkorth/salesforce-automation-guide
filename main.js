8d6acae610cb3279354fb1b123f71017f826556e
Michael Schmidt-Korth <mschmidtkorth@salesforce.com>
2020-06-12 17:25:35 +0200

    feat: Improved responsive design; improved theme; JQuery -> Boostrap

--- main.js
+++ main.js
@@ -29,3 +29,16 @@ function showDetails(elem, identifier) {
 	$('#' + identifier).toggle()
 	elem.innerHTML = elem.innerHTML == 'Show Order of Execution from an automation perspective' ? 'Hide Order of Execution from an automation perspective' : 'Show Order of Execution from an automation perspective';
 }
+
+function mobileFullscreen() {
+	$('#header').toggle();
+	$('#footer').toggle();
+	var maxButton = $('#maximize')
+	if (maxButton.html() == 'open_in_full') {
+		$('body').addClass('fullscreen');
+		maxButton.html('close_fullscreen');
+	} else {
+		$('body').removeClass('fullscreen');
+		maxButton.html('open_in_full');
+	}
+}
