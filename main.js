c3a0947b53b7fc4daf22c45e806fabe1dd9e7259
Michael Schmidt-Korth <mschmidtkorth@salesforce.com>
2020-06-12 21:45:51 +0200

    feat: Download charts; fix: Reordered charts sequence

--- main.js
+++ main.js
@@ -30,6 +30,11 @@ function showDetails(elem, identifier) {
 	elem.innerHTML = elem.innerHTML == 'Show Order of Execution from an automation perspective' ? 'Hide Order of Execution from an automation perspective' : 'Show Order of Execution from an automation perspective';
 }
 
+function showAllCharts() {
+	$('#charts').toggle();
+	$('#charts').html('<a href="ratingFlow.png" target="_blank"><img src="ratingFlow.png" alt="Rating for Flows." width="25%" /></a><a href="ratingProcess.png" target="_blank"><img src="ratingProcess.png" alt="Rating for Process Builder." width="25%" /></a><a href="ratingWorkflow.png" target="_blank"><img src="ratingWorkflow.png" alt="Rating for Wokflows." width="25%" /></a><a href="ratingApex.png" target="_blank"><img src="ratingApex.png" alt="Rating for Apex." width="25%" /></a>');
+}
+
 function mobileFullscreen() {
 	$('#header').toggle();
 	$('#footer').toggle();
@@ -42,3 +47,48 @@ function mobileFullscreen() {
 		maxButton.html('open_in_full');
 	}
 }
+
+// Convert d3 svg to png - issue: Does not work for multiple svgs; furthermore
+// Source: http://bl.ocks.org/vicapow/758fce6aa4c5195d24be
+// function svgAsPng() {
+// 	var doctype = '<?xml version="1.0" standalone="no"?>' +
+// 		'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+
+// 	// serialize our SVG XML to a string.
+// 	var source = (new XMLSerializer()).serializeToString(d3.select('svg').node());
+
+// 	// create a file blob of our SVG.
+// 	var blob = new Blob([doctype + source], {
+// 		type: 'image/svg+xml;charset=utf-8'
+// 	});
+
+// 	var url = window.URL.createObjectURL(blob);
+
+// 	// Put the svg into an image tag so that the Canvas element can read it in.
+// 	var img = d3.select('body').append('img')
+// 		.attr('width', 100)
+// 		.attr('height', 100)
+// 		.node();
+
+// 	img.onload = function () {
+// 		// Now that the image has loaded, put the image into a canvas element.
+// 		var canvas = d3.select('body').append('canvas').node();
+// 		canvas.width = 100;
+// 		canvas.height = 100;
+// 		var ctx = canvas.getContext('2d');
+// 		ctx.drawImage(img, 0, 0);
+// 		var canvasUrl = canvas.toDataURL("image/png");
+// 		var img2 = d3.select('body').append('img')
+// 			.attr('width', 100)
+// 			.attr('height', 100)
+// 			.node();
+// 		// this is now the base64 encoded version of our PNG! you could optionally
+// 		// redirect the user to download the PNG by sending them to the url with
+// 		// `window.location.href= canvasUrl`.
+// 		img2.src = canvasUrl;
+// 		console.log(canvasUrl)
+// 		//window.location.href = canvasUrl;
+// 	};
+// 	// start loading the image.
+// 	img.src = url;
+// }
